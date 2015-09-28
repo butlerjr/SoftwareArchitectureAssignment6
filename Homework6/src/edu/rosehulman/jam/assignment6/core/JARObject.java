bff3e01d229dc2221a84155d78ffb4fea5de1801
unknown <butlerjr@BUTLERJR-W530.rose-hulman.edu>
2015-09-28 18:35:05 -0400

    You can now get the instance of a JamDriver class implemented within a jar file. It must conform to the IPlugin interface.

--- Homework6/src/edu/rosehulman/jam/assignment6/core/JARObject.java
+++ Homework6/src/edu/rosehulman/jam/assignment6/core/JARObject.java
@@ -2,15 +2,26 @@ package edu.rosehulman.jam.assignment6.core;
 
 import java.nio.file.Path;
 
+import edu.rosehulman.jam.assignment6.IPlugin;
+
 public class JARObject {
 	
 	private Path filename;
 	private JarClassLoader jarClassLoader;
+	private Class pluginClass;
 
 	public JARObject(Path filename) {
 		//Any plugin is required to have a class that kicks things off called JamDriver
 		this.filename = filename;
 		this.jarClassLoader = new JarClassLoader(filename.toString());
+		try {
+			this.pluginClass = this.jarClassLoader.loadClass("JamDriver", true);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
 		/*
 		 * Then to instantiate an object you can do something like:
 		try {
@@ -39,5 +50,11 @@ public class JARObject {
 	public JarClassLoader getJarClassLoader(){
 		return this.jarClassLoader;
 	}
+	
+	public IPlugin newPluginInstance() throws InstantiationException, IllegalAccessException{
+		return (IPlugin) this.pluginClass.newInstance();
+	}
+	
+	
 
 }
